e7fc21aaf35f1aef322f1a34f06e1db73aad0ec7
TeamXpertidea <133506933+TeamXpertidea@users.noreply.github.com>
2024-07-31 12:39:38 +0530

    first desktop version push

--- index.php
+++ index.php
@@ -1,11 +1,309 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Index.php</title>
+	<title>Instant bookkeeper</title>
+	<link rel="stylesheet" href="./assets/stylesheet.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
+
 <body>
-	<h1>Index.php</h1>
+<nav class="navbar navbar-dark navbar-expand-sm  nav_des fixed-top">
+        <div class="container">
+        <a href="/" class="navbar-brand">
+        <img src="./assets/images/logo.png" alt="website_logo" class="main_logo">
+        </a>
+       
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="#home" class=" nav__link w-nav-link w--current-active">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#services" class="nav__link w-nav-link w--current ">
+                    Service
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#about" class=" nav__link w-nav-link w--current ">
+                    About
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#feature" class=" nav__link w-nav-link w--current ">
+                  Features
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#pricing" class=" nav__link w-nav-link w--current ">
+                  Pricing
+                </a>
+            </li>
+            <li  class="nav__link">
+            <a href="#contact-us" class=" nav-link_contact-us ">Contact Us</a>
+</li>
+        </ul>
+        </div>
+
+ </div>
+</nav>
+  <section class="main_banner" id="home">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-lg-8">
+        <div class="hero_content">
+        <div class="hero__content-wr"><h1 class="hero__ttl">Take Your Business to New Height</h1><p class="hero__subttl">If you're a Business Owner, you know what a hassle bookkeeping can be. You have likely spent far too many hours trying to figure out where you make the most money and what eats up your profits.</p><a href="#Services" class="button w-button">Our Services</a></div>  
+        </div>
+      </div>
+      <div class="col-md-4 col-lg-4"></div>
+    </div>
+  </div>
+</section>
+
+<section class="service" id="services">
+<div class="container"> 
+    <div class="row">
+    <div class="service__headline"><div class="headline__label">Services</div>
+    <h2 class="service_headline__ttl">That Best Solution For <br>Your Amazon Business<br></h2>
+    <p class="Service_headline__subttl">We take a unique approach to bookkeeping by giving your a clear and complette picture of your expenses and sales by item. You'll be able to determine the profit margins on a single item as well as the Cost of Goods sold and all of the expenses directly associated with that item. If you've used QuickBooks Online, you know that there is no simple way to do it.<br></p></div>
+    </div>
+    <div class="service_box">
+    <div class="row">
+        
+            <div class="col-md-3 col-lg-3 col-12" >
+            <div id="service_icon_box">
+            <div class="service__icon"><div class="service__text-icon"><i class="fa-solid fa-box"></i></div></div>
+            <h3 class="item-ttl item-ttl_service">Amazon - Ecommerce<br></h3>
+            <p class="item-text item-text_service">Amazon Bookkeeping: Our many years of experience in the Amazon business gives us the unique advantage of being able to help you with your amazon numbers. We help Amazon sift to the bottom of every, figure out their itemized profits, and become more profitable.<br>
+        </p></div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-12" >
+            <div id="service_icon_box">
+            <div class="service__icon"><div class="service__text-icon"><i class="fa-solid fa-earth-americas"></i></div></div>
+            <h3 class="item-ttl item-ttl_service">Real Estate<br></h3>
+            <p class="item-text item-text_service">If you're concerned bout FBA sales tax compliance, we'll manage yours sales tax process so that you don't have to worry about any audits.<br>
+        </p></div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-12" >
+            <div id="service_icon_box">
+            <div class="service__icon"><div class="service__text-icon"><i class="fa-solid fa-bookmark"></i></div></div>
+            <h3 class="item-ttl item-ttl_service">Professional Services<br></h3>
+            <p class="item-text item-text_service">if you have employees, we can handle all payroll processing on your behalf. We are well-versed in payroll laws and can take this time-consuming off your plate.<br>
+        </p></div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-12" >
+            <div id="service_icon_box">
+            <div class="service__icon"><div class="service__text-icon"><i class="fa-solid fa-cart-shopping"></i></div></div>
+            <h3 class="item-ttl item-ttl_service">Amazon - Ecommerce<br></h3>
+            <p class="item-text item-text_service">Get the exact report you need delivered to your inbox, weekly, bi-weekly, or mounthly<br>
+        </p></div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+<section class="about" id="about">
+<div class="container">
+    <div class="row"> 
+    <div class="section__headline section__headline_no-marg"><div class="headline__label">About</div><h2 class="headline__ttl">That Best Solution For <br>Your Amazon Business<br></h2><p class="headline__subttl">We take a unique approach to bookkeeping by giving your a clear and complette picture of your expenses and sales by item. You'll be able to determine the profit margins on a single item as well as the Cost of Goods sold and all of the expenses directly associated with that item. If you've used QuickBooks Online, you know that there is no simple way to do it.<br></p></div>
+    </div>
+</div>
+</section>
+<section class="card_about">
+<div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-6 col-12">
+        <div class="card_box_about">
+        <h3 class="item-ttl">Highest Quality</h3>
+        <p class="item-text">Instant Bookkeeper offers the highest quality bookkeeping services to Amazon sellers. We make it a breeze for you to see which items make you the most money, which Amazon fees are eating up the most profits, all within QuickBooks Online.</p>
+        </div>
+      </div>  
+      <div class="col-md-6 col-lg-6 col-12">
+      <div class="card_box_about">
+      <h3 class="item-ttl">Focus on Growing</h3>
+      <p class="item-text">With our specialized Amazon bookkeeping services, you can focus on your strengths: sourcing new products, tracking shipments, managing inventory, and earning seller reviews. You will no longer have wonder whether your numbers are accurate or IF business is growing</p>
+      </div>
+      </div>
+</div>
+</div> 
+</section>
+<section class="feature" id="feature">
+<div class="container">
+ <div class="feature_inner">
+<div class="row">
+    <div class="col-md-4 col-lg-4 col-12">
+     <div class="feature_img">
+     <img src="./assets/images/fe-img.jpeg" alt="feature" class="img_fe_des">
+     </div>
+    </div>
+    <div class="col-md-8 col-lg-8 col-12">
+    <div class="section__headline"><div class="headline__label">Features</div><h2 class="headline__ttl">Why Choose Instant Bookkeeper?<br></h2><p class="headline__subttl">Bookkeeping, Payroll, and taxes can all be stressful. We can handle all of these business tasks on your behalf so that you can actually enjoy running your Amazon business. There are countless benefits to outsourcing your Amazon business bookkeeping to us including:
+        
+    <br></p>
+    <div class="row">
+     <div class="col-md-6 col-12 pt-3">
+<div class="fe_in_small">
+<h3 class="item-ttl">Time Savings<br></h3>
+<p class="item-text">Let's be honest. Bookkeeping for Amazon sellers is complicated and time consuming. Rather than spending your precious time working with spreadsheets or trying to figure out how and what to input in QuickBooks, leave the bookkeeping to the professionals at Instant Bookkeeper</p>
+</div>
+     </div>  
+     <div class="col-md-6 col-12 pt-3">
+     <div class="fe_in_small">
+     <h3 class="item-ttl">Increased Profits<br></h3>
+     <p class="item-text">If your goal to make more money and ensure your Amazon business thrives, our bookkeeping services can help. You'll be able to see which items are working, which ones are not, and how you can maximize your ROI. You won't have to wonder how to grow your business because our bookkeeping services will have all the answers.</p>
+     </div>
+     </div> 
+    
+    <div>
+    </div>
+</div>
+</div>
+ </div>   
+</div>
+</section>
+
+<section class="logo_sec">
+    <div class="container">
+        <div class="logo_inner_content">
+         <div class="row">
+            <div class="col"> <div class="logo_client_des">  <img src="./assets/images/logo1.png" alt="client" class="img_logo_des"> </div></div>
+            <div class="col"> <div class="logo_client_des">  <img src="./assets/images/logo2.png" alt="client" class="img_logo_des"> </div></div>
+            <div class="col"> <div class="logo_client_des">  <img src="./assets/images/logo3.png" alt="client" class="img_logo_des"> </div></div>
+            <div class="col"> <div class="logo_client_des">  <img src="./assets/images/logo5.png" alt="client" class="img_logo_des"> </div></div>
+            <div class="col"> <div class="logo_client_des">  <img src="./assets/images/logo6.jpg" alt="client" class="img_logo_des"> </div></div>
+</div>
+        </div>
+        </div>
+    </div>
+
+</section>
+<section class="pricing" id="pricing">
+    <div class="container">
+        <div class="row">
+        <div class="section__headline section__headline_grey">
+        <div class="headline__label">Pricing</div><h2 class="headline__ttl">Reach Your Potential Faster<br></h2>
+        <p class="headline__subttl mb-5">We will take care of all of your accounting and administrative costs so that you can focus on growing your business<br></p>
+        <div class="check-pricing__wr mt-3"><a href="#contact-us" id="" class="button_outline_pricing ">CONTACT&nbsp;US</a></div></div>
+        </div>
+    </div>
+</section>
+
+
+<section class="contact_us" id="contact-us">
+    <div class="container">
+        <div class="row">
+        <div class=" section__headline_contact-us"><div class="headline__label headline__label_cta">Contact us</div><h2 class="headline__ttl headline__ttl_wht">Contact US TODAY<br></h2><p class="headline__subttl headline__subttl_cta">For more information on how our services can help your Amazon profits skyrocket, we encourage you to contact us today. Feel free to call us at <a href="tel:845-554-5542" class="wht-link"><span>845-554-5542</span></a> or fill out our convenient online contact form<br></p></div>
+        </div>
+    </div>
+</section>
+<section class="frm_contact">
+    <div class="container">
+        <div class="row">
+            <div class="form_sec_box">
+            <form action="#" class="form_desi">
+
+            <div class="form__iconhh"><div class="form__icon"><i class="fa-regular fa-envelope"></i><br></div></div>
+            <h3 class="item-ttl item-ttl_contact-us-form">Get In Touch<br></h3>
+            <div class="mb-3 mt-3">
+      
+      <input type="name" class="form-control" id="name" placeholder="Name / Company name" name="name">
+    </div>
+    <div class="mb-3 mt-3">
+      
+      <input type="email" class="form-control" id="email" placeholder="e-mail" name="email">
+    </div>
+   
+    <div class="mb-3 mt-3">
+    <textarea class="form-control" rows="5" id="comment" name="text" placeholder="Message"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="sec_footer">
+    
+
+<div class="container">
+    <div class="main_footer_content">
+    <div class="row">
+        <div class="col-md-4 col-lg-4 col-12">
+        <div id="img_footer" class="column_footer footer_d1">
+        <img src="./assets/images/logo.png" alt="" class="logo_footer">
+        </div>  
+        </div>
+
+
+        <div class="col-md-3 col-lg-3 col-12">
+        <div id="img_footer" class="column_footer footer_d1">
+
+        <h4>Contact info</h4>
+     <div class="footer__item-row">
+      <div class="footer__item-icon"><i class="fa-solid fa-location-dot"></i></div>
+      <p class="contact__text">Instant Bookkeeper Inc<br>2 Perlman Dr Suite 205<br>Spring Valley, NY 10977</p></div>
+    <div class="footer__item-row">
+    <div class="footer__item-icon"><i class="fa-solid fa-phone"></i></div>
+    <a href="tel:845-554-5542" class="contact__text contact__text_link">845-554-5542</a></div>
+    <div class="footer__item-row"><div class="footer__item-icon"><i class="fa-regular fa-envelope"></i></div><a href="mailto:info@instantbookkeeper.com" class="contact__text contact__text_link">
+    <span>info@instantbookkeeper.com</span></a></div>
+    <div class="footer__item-row"><div class="footer__item-icon"><i class="fa-brands fa-internet-explorer"></i></div>
+    <a href="#" class="contact__text contact__text_link">https://instantbookkeeper.com</a></div>
+
+
+        </div>
+        </div>
+
+
+        <div class="col-md-4 col-lg-4 col-12"><div id="img_footer" class="column_footer footer_d1">
+        <h4>Working hours</h4>
+      <div class="footer__text footer__text_gh">We work whenever we can, but our phone works on the following schedule.</div>
+      <div class="footer__text footer__text_week">Monday - Friday: 9:00 - 17:00<br>Sunday and holidays: 12:00 - 03:00<br>Saturday: Closed</div>
+
+        </div>
+       </div>
+
+
+
+
+        <div class="col-md-1 col-lg-1 col-12"><div id="img_footer" class="column_footer footer_d4">
+        <h4>Social</h4>
+        <div class="social_icon_footer">
+        <a href="#" target="_blank" class="social__icon "><div><i class="fa-brands fa-twitter"></i></div></a>
+        <a href="#" target="_blank" class="social__icon "><div><i class="fa-brands fa-facebook-f"></i></div></a>
+        <a href="#" target="_blank" class="social__icon "><div><i class="fa-brands fa-skype"></i></div></a>
+        <a href="#" target="_blank" class="social__icon "><div><i class="fa-brands fa-linkedin-in"></i></div></a>
+        <a href="#" target="_blank" class="social__icon "><div><i class="fa-solid fa-basketball"></i></div></a>
+        </div>
+
+
+        </div></div>
+
+
+
+
+    </div>
+    </div>
+</div>
+
+</footer>
+<section class="footer_copy_right">
+<div class="container">
+<p class="footer__copy">© 2024, Instant Bookkeeper Inc, All rights reserved</p>
+<a href="/privacy-policy" target="_blank" class="link">Privacy Policy</a></div>
+</section>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </html>
